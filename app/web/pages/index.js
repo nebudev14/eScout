c153398f4aa9b35fa0395f6821925fa94565737c
warren yun <wyun2006@gmail.com>
2022-06-24 13:31:43 -0400

    init web

---
+++ app/web/pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-2xl">Hello</h1>
+    </div>
+  );
+}
