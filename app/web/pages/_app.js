7cf9561a7ce69dd9108e2d9c2b1a97724817e3cd
warren yun <wyun2006@gmail.com>
2022-06-24 17:54:38 -0400

    basic auth

--- app/web/pages/_app.js
+++ app/web/pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="bg-black">
+      <Component {...pageProps} />{" "}
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
