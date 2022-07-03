2a42000d376e9489c82f7c84f314493d4211df7c
warren yun <wyun2006@gmail.com>
2022-07-03 01:55:29 -0400

    updated protected component

--- components/auth/Protected.tsx
+++ components/auth/Protected.tsx
@@ -1,1 +1,12 @@
 import { signIn, useSession } from "next-auth/react";
+import React from "react";
+
+export const Protected: React.FC<React.PropsWithChildren<{}>> = ({
+  children,
+}) => {
+  const { status } = useSession();
+  if (status === "unauthenticated") signIn("google");
+  if (status === "authenticated") return <>{children}</>;
+
+  return null;
+};
