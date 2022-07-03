0f652a5f119457612b35771fe1f65faaf128921a
warren yun <wyun2006@gmail.com>
2022-07-03 02:01:46 -0400

    set up auth + new index page with ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { Nav } from "../components/Nav";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <div className="bg-slate-100">
+        <Nav />
+        <Component {...pageProps} />{" "}
+      </div>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,35 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Link from "next/link";
+import { Protected } from "../components/auth/Protected";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
-  return (
-    <div>
-    </div>
-  )
-}
+  const { data: session } = useSession();
 
-export default Home
+  return (
+    <Protected>
+      <div className="flex flex-col items-center justify-center h-screen">
+        {/* <button
+            className="p-4 mb-4 text-white duration-200 border border-green-400 rounded-xl hover:border-green-400"
+            onClick={() => signIn()}
+          >
+            Sign in
+          </button> */}
+        <Link href="/scout" passHref>
+          <button
+            className="px-4 py-2 mb-4 text-3xl text-white duration-200 bg-teal-500 border rounded-md hover:bg-teal-700"
+            type="button"
+          >
+            Scout!
+          </button>
+        </Link>
+        <button className="px-8 py-4 text-xl text-white duration-200 bg-purple-500 border rounded-lg hover:bg-purple-400">
+          View scouting data
+        </button>
+      </div>
+      )
+    </Protected>
+  );
+};
+
+export default Home;
