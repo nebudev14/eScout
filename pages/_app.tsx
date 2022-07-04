238e0639124a4a7be240d60fdf8c3e066565a3b1
warren yun <wyun2006@gmail.com>
2022-07-04 01:50:59 -0400

    more trpc setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,42 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import { Nav } from "../components/Nav";
+import { Nav } from "../components/nav";
 import "../styles/globals.css";
+import { withTRPC } from "@trpc/next";
+import type { AppRouter } from "../server/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <div className="bg-slate-100">
         <Nav />
-        <Component {...pageProps} />{" "}
+        <Component {...pageProps} />
       </div>
     </SessionProvider>
   );
 }
 
-export default MyApp;
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : 'http://localhost:3000/api/trpc';
+
+    return {
+      url,
+      /**
+       * @link https://react-query.tanstack.com/reference/QueryClient
+       */
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+    };
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  ssr: true,
+})(MyApp);
