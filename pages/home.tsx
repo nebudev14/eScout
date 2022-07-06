e4709af5fc76424bdffba9c9be524d3991614717
warren yun <wyun2006@gmail.com>
2022-07-06 10:26:38 -0400

    new index page

---
+++ pages/home.tsx
@@ -0,0 +1,46 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+import { Protected } from "../components/auth/protected";
+import { useMutation } from "../hooks/trpc";
+import { useSession } from "next-auth/react";
+
+const Home: NextPage = () => {
+  const user = useMutation("user.create");
+
+  return (
+    <Protected>
+      <div className="flex flex-col items-center justify-center h-screen">
+        {/* <button
+            className="p-4 mb-4 text-white duration-200 border border-green-400 rounded-xl hover:border-green-400"
+            onClick={() => signIn()}
+          >
+            Sign in
+          </button> */}
+        <Link href="/scout" passHref>
+          <button
+            className="px-4 py-2 mb-4 text-3xl text-white duration-200 bg-teal-500 border rounded-md hover:bg-teal-700"
+            type="button"
+          >
+            Scout!
+          </button>
+        </Link>
+        <button
+          className="px-8 py-4 text-xl text-white duration-200 bg-purple-500 border rounded-lg hover:bg-purple-400"
+          // onClick={async () => {
+          //   await user.mutateAsync({
+          //     id: "b",
+          //     name: "warren",
+          //     email: "dasf",
+          //     image: "asdf",
+          //   });
+          // }}
+        >
+          View scouting data
+        </button>
+      </div>
+      )
+    </Protected>
+  );
+};
+
+export default Home;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,26 @@
 import type { NextPage } from "next";
-import Link from "next/link";
-import { Protected } from "../components/auth/protected";
-import { useMutation } from "../hooks/trpc";
-import { useSession } from "next-auth/react";
+import { BsGoogle } from "react-icons/bs";
+import { UnAuthed } from "../components/auth/unauthed";
+import { signIn } from "next-auth/react";
 
-const Home: NextPage = () => {
-  const user = useMutation("user.create");
-
+const SignIn: NextPage = () => {
   return (
-    <Protected>
+    <UnAuthed path="/home">
       <div className="flex flex-col items-center justify-center h-screen">
-        {/* <button
-            className="p-4 mb-4 text-white duration-200 border border-green-400 rounded-xl hover:border-green-400"
-            onClick={() => signIn()}
-          >
-            Sign in
-          </button> */}
-        <Link href="/scout" passHref>
-          <button
-            className="px-4 py-2 mb-4 text-3xl text-white duration-200 bg-teal-500 border rounded-md hover:bg-teal-700"
-            type="button"
-          >
-            Scout!
-          </button>
-        </Link>
+        <h1 className="mb-6 text-3xl">eScout</h1>
         <button
-          className="px-8 py-4 text-xl text-white duration-200 bg-purple-500 border rounded-lg hover:bg-purple-400"
-          // onClick={async () => {
-          //   await user.mutateAsync({
-          //     id: "b",
-          //     name: "warren",
-          //     email: "dasf",
-          //     image: "asdf",
-          //   });
-          // }}
+          className="px-8 py-4 text-2xl text-white duration-200 bg-purple-500 border rounded-lg hover:bg-purple-400"
+          onClick={() => signIn("google")}
         >
-          View scouting data
+          <div className="flex flex-row items-center justify-center text-center">
+            <h1 className="mr-4">Sign in with </h1>
+            <BsGoogle className="text-3xl" />
+          </div>
         </button>
       </div>
       )
-    </Protected>
+    </UnAuthed>
   );
 };
 
-export default Home;
+export default SignIn;
