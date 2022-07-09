ddbb5ac81a171ebf08045c758a47c6eaf15fec88
warren yun <wyun2006@gmail.com>
2022-07-09 01:03:41 -0400

    buttons to adjust shot scoreboard

--- pages/scout.tsx
+++ pages/scout.tsx
@@ -40,9 +40,35 @@ const Scout: NextPage = () => {
             </label>
             <div className="flex items-center text-lg font-medium leading-tight border rounded shadow focus:outline-none focus:shadow-outline justify-evenly">
               <h1>0</h1>
-              <h1 className="flex items-center justify-center h-full px-4 text-center border-l-2 border-r-2 border-slate-300" >/</h1>
+              <h1 className="flex items-center justify-center h-full px-4 text-center border-l-2 border-r-2 border-slate-300">
+                /
+              </h1>
               <h1>0</h1>
             </div>
+          </Container>
+          <Container>
+            <Container>
+              <button className="p-2 text-lg font-semibold text-white bg-green-500 rounded-tl rounded-bl shadow focus:outline-none focus:shadow-outline hover:bg-teal-700">
+                Score
+              </button>
+              <button className="p-2 text-lg font-semibold text-white bg-red-500 border-r-2 rounded-tr rounded-br shadow border- focus:outline-none focus:shadow-outline hover:bg-teal-700">
+                Miss
+              </button>
+            </Container>
+            <Container>
+              <button
+                type="button"
+                className="p-2 text-lg font-semibold text-white bg-yellow-500 border-r-2 border-white rounded-tl rounded-bl shadow focus:outline-none focus:shadow-outline hover:bg-teal-700"
+              >
+                -1
+              </button>
+              <button
+                type="button"
+                className="p-2 text-lg font-semibold text-white bg-yellow-500 rounded-tr rounded-br shadow focus:outline-none focus:shadow-outline hover:bg-teal-700"
+              >
+                -1
+              </button>
+            </Container>
           </Container>
           <button
             type="submit"
