5d9f040a453c8050831682d365a413c7d544e861
warren yun <wyun2006@gmail.com>
2022-07-09 05:11:32 -0400

    endgame section

--- pages/scout.tsx
+++ pages/scout.tsx
@@ -22,7 +22,7 @@ const Scout: NextPage = () => {
       <div className="h-full px-56 py-4 lg:px-4">
         <form onSubmit={submitData} className="grid grid-cols-1">
           <MatchInfo />
-          <h1 className="mb-2 text-3xl font-semibold">Auto</h1>
+          <h1 className="mb-4 text-3xl font-semibold">Auto</h1>
           <Container>
             <label className="p-2 text-lg font-semibold leading-tight border rounded shadow bg-slate-200 focus:outline-none focus:shadow-outline">
               Mobility
@@ -47,9 +47,10 @@ const Scout: NextPage = () => {
             </label>
             <div className="flex">
               <input
+                className="w-10/12 h-full p-2 text-lg border rounded-l border-blue-lighter"
                 type="text"
                 placeholder="Team number"
-                className="w-10/12 h-full p-2 text-lg border rounded-l border-blue-lighter"
+                autoComplete="off"
               />
               <button className="flex items-center justify-center px-2 text-white bg-blue-500 border-t border-b border-l rounded-r p bg-blue-lighter border-blue-lighter text-blue-dark">
                 +
@@ -62,16 +63,54 @@ const Scout: NextPage = () => {
             </label>
             <div className="flex">
               <input
+                className="w-10/12 h-full p-2 text-lg border rounded-l border-blue-lighter"
                 type="text"
                 placeholder="Team number"
-                className="w-10/12 h-full p-2 text-lg border rounded-l border-blue-lighter"
+                autoComplete="off"
               />
               <button className="flex items-center justify-center px-2 text-white bg-blue-500 border-t border-b border-l rounded-r p bg-blue-lighter border-blue-lighter text-blue-dark">
                 +
               </button>
             </div>
           </Container>
-
+          <h1 className="my-4 text-3xl font-semibold ">Endgame</h1>
+          <Container>
+            <label className="p-2 text-lg leading-tight border rounded shadow bg-slate-200 focus:outline-none focus:shadow-outline">
+              Climb start time
+            </label>
+            <Input
+              id="climbStart"
+              placeholder="Start time"
+              type="number"
+              autoComplete="off"
+            />
+          </Container>
+          <Container>
+            <label className="p-2 text-lg leading-tight border rounded shadow bg-slate-200 focus:outline-none focus:shadow-outline">
+              Climb end time
+            </label>
+            <Input
+              id="climbEnd"
+              placeholder="End time"
+              type="number"
+              autoComplete="off"
+            />
+          </Container>
+          <Container>
+            <label className="p-2 text-lg leading-tight border rounded shadow bg-slate-200 focus:outline-none focus:shadow-outline">
+              Climb rung
+            </label>
+            <select
+              id="climbBar"
+              className="p-2 text-lg leading-tight border rounded shadow focus:outline-none focus:shadow-outline"
+            >
+              <option>None</option>
+              <option>Low</option>
+              <option>Mid</option>
+              <option>High</option>
+              <option>Traversal</option>
+            </select>
+          </Container>
           <button
             type="submit"
             className="p-2 mt-4 text-lg font-semibold text-white bg-teal-500 rounded shadow focus:outline-none focus:shadow-outline hover:bg-teal-700"
