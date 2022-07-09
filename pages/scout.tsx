a13654da546de9a62bed6356165594bdad8e819a
warren yun <wyun2006@gmail.com>
2022-07-09 05:18:03 -0400

    comments

--- pages/scout.tsx
+++ pages/scout.tsx
@@ -41,22 +41,24 @@ const Scout: NextPage = () => {
           <ScoreBoard label="High Goal" id="teleopHighGoal" />
           <ScoreBoard label="Low Goal" id="teleopLowGoal" />
           <h1 className="my-4 text-3xl font-semibold ">Defense</h1>
-          <Container>
-            <label className="flex items-center justify-start p-2 text-lg leading-tight border rounded shadow bg-slate-200 focus:outline-none focus:shadow-outline">
-              Defended
-            </label>
-            <div className="flex">
-              <input
-                className="w-10/12 h-full p-2 text-lg border rounded-l border-blue-lighter"
-                type="text"
-                placeholder="Team number"
-                autoComplete="off"
-              />
-              <button className="flex items-center justify-center px-2 text-white bg-blue-500 border-t border-b border-l rounded-r p bg-blue-lighter border-blue-lighter text-blue-dark">
-                +
-              </button>
-            </div>
-          </Container>
+          <div className="mb-2">
+            <Container>
+              <label className="flex items-center justify-start p-2 text-lg leading-tight border rounded shadow bg-slate-200 focus:outline-none focus:shadow-outline">
+                Defended
+              </label>
+              <div className="flex">
+                <input
+                  className="w-10/12 h-full p-2 text-lg border rounded-l border-blue-lighter"
+                  type="text"
+                  placeholder="Team number"
+                  autoComplete="off"
+                />
+                <button className="flex items-center justify-center px-2 text-white bg-blue-500 border-t border-b border-l rounded-r p bg-blue-lighter border-blue-lighter text-blue-dark">
+                  +
+                </button>
+              </div>
+            </Container>
+          </div>
           <Container>
             <label className="flex items-center justify-start p-2 text-lg leading-tight border rounded shadow bg-slate-200 focus:outline-none focus:shadow-outline">
               Defended by
@@ -74,28 +76,32 @@ const Scout: NextPage = () => {
             </div>
           </Container>
           <h1 className="my-4 text-3xl font-semibold ">Endgame</h1>
-          <Container>
-            <label className="p-2 text-lg leading-tight border rounded shadow bg-slate-200 focus:outline-none focus:shadow-outline">
-              Climb start time
-            </label>
-            <Input
-              id="climbStart"
-              placeholder="Start time"
-              type="number"
-              autoComplete="off"
-            />
-          </Container>
-          <Container>
-            <label className="p-2 text-lg leading-tight border rounded shadow bg-slate-200 focus:outline-none focus:shadow-outline">
-              Climb end time
-            </label>
-            <Input
-              id="climbEnd"
-              placeholder="End time"
-              type="number"
-              autoComplete="off"
-            />
-          </Container>
+          <div className="mb-2">
+            <Container>
+              <label className="p-2 text-lg leading-tight border rounded shadow bg-slate-200 focus:outline-none focus:shadow-outline">
+                Climb start time
+              </label>
+              <Input
+                id="climbStart"
+                placeholder="Start time"
+                type="number"
+                autoComplete="off"
+              />
+            </Container>
+          </div>
+          <div className="mb-2">
+            <Container>
+              <label className="p-2 text-lg leading-tight border rounded shadow bg-slate-200 focus:outline-none focus:shadow-outline">
+                Climb end time
+              </label>
+              <Input
+                id="climbEnd"
+                placeholder="End time"
+                type="number"
+                autoComplete="off"
+              />
+            </Container>
+          </div>
           <Container>
             <label className="p-2 text-lg leading-tight border rounded shadow bg-slate-200 focus:outline-none focus:shadow-outline">
               Climb rung
@@ -111,6 +117,8 @@ const Scout: NextPage = () => {
               <option>Traversal</option>
             </select>
           </Container>
+          <h1 className="my-4 text-3xl font-semibold ">Comments</h1>
+          <textarea id="comments" className="p-4 border rounded-xl border-slate-300" autoComplete="off" rows={10}  />
           <button
             type="submit"
             className="p-2 mt-4 text-lg font-semibold text-white bg-teal-500 rounded shadow focus:outline-none focus:shadow-outline hover:bg-teal-700"
