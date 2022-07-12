0d3a497b288ddc8d26c1ae410daaa464a11c916a
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2022-07-11 22:30:01 -0400

    Created Entry schema and router. Hello

--- server/routers/app.ts
+++ server/routers/app.ts
@@ -2,10 +2,12 @@ import { createRouter }from "../create-router";
 import { userRouter } from "./user-router";
 import { teamRouter } from "./team-router";
 import { inviteRouter } from "./invite-router";
+import { entryRouter } from "./entry-route";
 
 export const appRouter = createRouter()
     .merge("user.", userRouter)
     .merge("team.", teamRouter)
-    .merge("invite.", inviteRouter);
+    .merge("invite.", inviteRouter)
+    .merge("entry.", entryRouter);
 
 export type AppRouter = typeof appRouter;
---
+++ server/routers/entry-route.ts
@@ -0,0 +1,41 @@
+import { createRouter } from "../create-router";
+import { createEntrySchema } from "../schemas/entry-schema";
+
+export const entryRouter = createRouter()
+  .mutation("create", {
+    input: createEntrySchema,
+    async resolve({ input, ctx }) {
+        console.log("danny was here");
+        return await ctx.prisma.entry.create({
+            data: {
+                id : input.id,
+                teamNumber : input.teamNumber,
+
+                matchNumber : input.matchNumber,
+                matchType : input.matchType,
+                eventName : input.eventName,
+
+                mobility : input.mobility,
+                
+                autoHighShotsMade: input.autoHighShotsMade,
+                autoHighShotsTotal: input.autoHighShotsTotal,
+                autoLowShotsMade: input.autoLowShotsMade,
+                autoLowShotsTotal: input.autoLowShotsTotal,
+            
+                teleopHighShotsMade: input.teleopHighShotsMade,
+                teleopHighShotsTotal: input.teleopHighShotsTotal,
+                teleopLowShotsMade: input.teleopLowShotsMade,
+                teleopLowShotsTotal: input.teleopLowShotsTotal,
+            
+                climbStart: input.climbStart,
+                climbEnd: input.climbEnd,
+                climbRung: input.climbRung,
+            
+                defended: input.defended,
+                defendedBy: input.defendedBy,
+            
+                comments: input.comments
+            }
+        });
+    }
+});
