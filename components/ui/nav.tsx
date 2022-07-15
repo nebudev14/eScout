33ea2dfaeaf85d232e90aa0cf43cbe2f1088d115
warren yun <wyun2006@gmail.com>
2022-07-15 01:12:04 -0400

    holy this navbar is HOT

--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -1,5 +1,9 @@
 import { useSession, signOut } from "next-auth/react";
 import { BiLogOut, BiMenu } from "react-icons/bi";
+import { IoMdBuild } from "react-icons/io";
+import { FaUserFriends } from "react-icons/fa";
+import { GrNotes } from "react-icons/gr";
+import { MdAnalytics } from "react-icons/md";
 import { Menu } from "@headlessui/react";
 
 export const Nav: React.FC = () => {
@@ -19,27 +23,23 @@ export const Nav: React.FC = () => {
                 </Menu.Button>
               </div>
 
-              <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right divide-y divide-gray-100 rounded-md shadow-lg bg-slate-100 ring-1 ring-black ring-opacity-5 focus:outline-none">
                 <div className="px-1 py-1 ">
                   <Menu.Item>
                     {({ active }) => (
                       <button
-                        className={`${
-                          active ? "bg-violet-500 text-white" : "text-gray-900"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        className="flex items-center w-full px-2 py-2 text-sm text-black duration-200 rounded-md group hover:text-white hover:bg-black"
                       >
-                        Edit
+                        <IoMdBuild className="mr-2" /> Manage teams
                       </button>
                     )}
                   </Menu.Item>
                   <Menu.Item>
                     {({ active }) => (
                       <button
-                        className={`${
-                          active ? "bg-violet-500 text-white" : "text-gray-900"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        className="flex items-center w-full px-2 py-2 text-sm text-yellow-600 duration-200 rounded-md group hover:text-white hover:bg-yellow-500"
                       >
-                        Duplicate
+                        <FaUserFriends className="mr-2" /> Join a team
                       </button>
                     )}
                   </Menu.Item>
@@ -48,22 +48,18 @@ export const Nav: React.FC = () => {
                   <Menu.Item>
                     {({ active }) => (
                       <button
-                        className={`${
-                          active ? "bg-violet-500 text-white" : "text-gray-900"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        className="flex items-center w-full px-2 py-2 text-sm text-purple-500 duration-200 rounded-md group hover:text-white hover:bg-purple-400"
                       >
-                        Archive
+                        <GrNotes className="mr-2" /> Scout
                       </button>
                     )}
                   </Menu.Item>
                   <Menu.Item>
                     {({ active }) => (
                       <button
-                        className={`${
-                          active ? "bg-violet-500 text-white" : "text-gray-900"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        className="flex items-center w-full px-2 py-2 text-sm text-green-500 duration-200 rounded-md group hover:text-white hover:bg-green-500"
                       >
-                        Move
+                        <MdAnalytics className="mr-2" /> View scout data
                       </button>
                     )}
                   </Menu.Item>
@@ -75,7 +71,7 @@ export const Nav: React.FC = () => {
                       onClick={() => signOut()}
                         className="flex items-center w-full px-2 py-2 text-sm text-red-500 duration-200 rounded-md group hover:text-white hover:bg-red-400"
                       >
-                        <BiLogOut className="mr-2 text-2xl duration-200" /> Log out 
+                        <BiLogOut className="mr-2 duration-200" /> Log out 
                       </button>
                     )}
                   </Menu.Item>
