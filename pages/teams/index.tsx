9ffcd23643b7c08170599ad2eb6189ff79dc8df1
warren yun <wyun2006@gmail.com>
2022-07-20 00:31:29 -0400

    create team modal

--- pages/teams/index.tsx
+++ pages/teams/index.tsx
@@ -2,9 +2,9 @@ import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import { Protected } from "../../components/auth/protected";
 import { useQuery } from "../../hooks/trpc";
-import { useState } from "react";
-import { Dialog } from "@headlessui/react";
-
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Input } from "../../components/ui/input";
 
 const ManageTeams: NextPage = () => {
   const { data: session } = useSession();
@@ -13,16 +13,18 @@ const ManageTeams: NextPage = () => {
     { userId: session?.user?.id as string },
   ]);
 
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   if (isLoading || !data) return <h1>Loading..</h1>;
-  
-  
+
   return (
     <Protected>
       <div className="flex flex-col h-screen px-6 py-4">
         <div className="flex items-start justify-start">
-          <button className="px-3 py-2 mb-8 text-2xl text-white duration-200 bg-purple-500 rounded-md hover:bg-purple-600">
+          <button
+            className="px-3 py-2 mb-8 text-sm text-white duration-200 bg-purple-500 rounded-md hover:bg-purple-600"
+            onClick={() => setIsOpen(true)}
+          >
             Create
           </button>
         </div>
@@ -34,6 +36,70 @@ const ManageTeams: NextPage = () => {
           <h1>sike</h1>
         )}
       </div>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => setIsOpen(false)}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex items-center justify-center min-h-full p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-center text-gray-900"
+                  >
+                    Create a team
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500"></p>
+                  </div>
+
+                  <div className="mt-4 ">
+                    <h1 className="mr-2 font-semibold">Team name</h1>
+                    <input className="w-full p-1 border-2 rounded-lg outline-none" />
+                  </div>
+
+                  <div className="mt-4 ">
+                    <h1 className="mr-2 font-semibold">Team number</h1>
+                    <input className="w-full p-1 border-2 rounded-lg outline-none" type="number" />
+                  </div>
+
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-green-400 border border-transparent rounded-md hover:bg-purple-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={() => setIsOpen(false)}
+                    >
+                      Create
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </Protected>
   );
 };
