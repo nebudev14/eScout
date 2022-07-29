6867aa78a0ffa59cb857e1742d8234686259173e
warren yun <wyun2006@gmail.com>
2022-07-29 11:03:56 -0400

    WEE filtered cards render

--- components/scouting/filter.tsx
+++ components/scouting/filter.tsx
@@ -2,32 +2,31 @@ import { trpc, useQuery } from "../../hooks/trpc";
 import React, { useRef, useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 
-export const Filter: React.FC<{ teamNum: number }> = ({teamNum}) => {
+export const Filter: React.FC<{ teamNum: number }> = ({ teamNum }) => {
   interface Query {
-    teamNumber: number
+    teamNumber: number;
     entryTeamNumber?: number;
     eventName?: string;
   }
-  
+
   const teamNumber = useRef<HTMLInputElement>(null);
   const eventName = useRef<HTMLInputElement>(null);
-  
+
   const [input, setInput] = useState({
-    teamNumber: teamNum
+    teamNumber: teamNum,
   });
   const { data: entryData } = useQuery(["entry.get-by-filter", input]);
   const { invalidateQueries } = trpc.useContext();
 
   const searchEntry = async (event: React.SyntheticEvent) => {
     event.preventDefault();
-    const newInput: Query = {
-      teamNumber: teamNum
-    }
-    if (Number(teamNumber.current?.value) !== 0) newInput.entryTeamNumber = Number(teamNumber.current?.value);
-    if (eventName.current?.value !== "") newInput.eventName = eventName.current?.value;
+    const newInput: Query = { teamNumber: teamNum };
+    if (Number(teamNumber.current?.value) !== 0)
+      newInput.entryTeamNumber = Number(teamNumber.current?.value);
+    if (eventName.current?.value !== "")
+      newInput.eventName = eventName.current?.value;
     setInput(newInput);
     invalidateQueries("entry.get-by-filter");
-    console.log(entryData)
   };
 
   return (
@@ -43,7 +42,10 @@ export const Filter: React.FC<{ teamNum: number }> = ({teamNum}) => {
         ))}
       </div> */}
       <div className="flex flex-col items-center mb-4 shadow-sm justfiy-center">
-        <form className="flex items-center justify-center mb-6" onSubmit={searchEntry}>
+        <form
+          className="flex items-center justify-center mb-6"
+          onSubmit={searchEntry}
+        >
           <input
             className="w-full p-2 border-r-4 rounded-l-lg shadow-md outline-none"
             ref={teamNumber}
@@ -58,16 +60,47 @@ export const Filter: React.FC<{ teamNum: number }> = ({teamNum}) => {
             autoComplete="off"
           />
           <button
-            className="p-2 text-xl text-white bg-pink-600 rounded-r-lg shadow-md"
+            className="p-2 text-xl text-white bg-yellow-500 rounded-r-lg shadow-md"
             type="submit"
           >
             <AiOutlineSearch />
           </button>
         </form>
-        <div className="grid grid-cols-1">
+        <div className="grid w-full grid-cols-1 mb-4">
           {entryData?.map((entry, i) => (
-            <div key={i}>
-              <h1>{entry.autoHighShotsMade}</h1>
+            <div
+              className="mb-4 border rounded-lg shadow-md bg-slate-50"
+              key={i}
+            >
+              <div className="grid grid-cols-2 px-6 py-4">
+                <div>
+                  <h1>Team {entry?.entryTeamNumber}</h1>
+                  <h1 className="text-xl">{entry?.eventName}</h1>
+                </div>
+                <div className="flex flex-col">
+                  <div>
+                    <b>{entry.autoHighShotsMade + entry.autoLowShotsMade}</b>{" "}
+                    total auto goals
+                  </div>
+                  <div>
+                    <b>
+                      {entry.teleopHighShotsMade + entry.teleopLowShotsMade}
+                    </b>{" "}
+                    total teleop goals
+                  </div>
+                  <div>
+                    {entry.climbEnd - entry.climbStart === 0
+                      ? "No climb"
+                      : entry.climbStart - entry.climbEnd + " second climb"}
+                  </div>
+                  <div>
+                    {entry.climbRung} Rung
+                  </div>
+                </div>
+              </div>
+              <div className="py-1 text-sm text-center text-white bg-black rounded-b-lg">
+                {entry.matchType} {entry.matchNumber}
+              </div>
             </div>
           ))}
         </div>
