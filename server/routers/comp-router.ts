b5a88ff07456eeeb6df82b50a7f56b60d5588758
warren yun <wyun2006@gmail.com>
2022-08-01 03:18:12 -0400

    compettiion router

--- server/routers/comp-router.ts
+++ server/routers/comp-router.ts
@@ -1,2 +1,18 @@
+import { z } from "zod";
 import { createRouter } from "../create-router";
 
+export const compRouter = createRouter()
+  .mutation("create", {
+    input: z.object({
+      name: z.string(),
+      team: z.number()
+    }),
+    async resolve({ input, ctx }) {
+      return await ctx.prisma.competition.create({
+        data: {
+          name: input.name,
+          teamNumber: input.team
+        }
+      })
+    }
+  })
