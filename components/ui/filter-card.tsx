60f29bdaea013568900e6c98c5b112856835dd70
warren yun <wyun2006@gmail.com>
2022-10-18 00:16:04 -0400

    update filter card, fix calculation

--- components/ui/filter-card.tsx
+++ components/ui/filter-card.tsx
@@ -1,6 +1,13 @@
 import { Entry } from "@prisma/client";
 
 export const FilterCard: React.FC<{ entry: Entry }> = ({ entry }) => {
+  const totalAuto = entry.autoHighShotsMade + entry.autoLowShotsMade;
+  const totalTeleop = entry.teleopHighShotsMade + entry.teleopLowShotsMade;
+  const totalShots =
+    entry.autoHighShotsTotal +
+    entry.autoLowShotsTotal +
+    entry.teleopHighShotsTotal +
+    entry.teleopLowShotsTotal;
   return (
     <div className="py-2 mb-6 border shadow-xl rounded-xl bg-slate-50">
       <div className="grid grid-cols-2 px-5 py-4">
@@ -9,32 +16,23 @@ export const FilterCard: React.FC<{ entry: Entry }> = ({ entry }) => {
             {entry.matchType} {entry?.matchNumber}
           </h1>
           <h1>{entry.compName}</h1>
-          <h1 className="text-xl">Team {entry?.entryTeamNumber}</h1>
+          <h1 className="text-xl">
+            Team <b>{entry?.entryTeamNumber}</b>
+          </h1>
+          <div>
+            {(((totalAuto + totalTeleop) / totalShots) * 100).toFixed(2)}%
+            accuracy
+          </div>
         </div>
+
         <div className="flex flex-col text-right">
           <div>
-            <b>{entry.autoHighShotsMade + entry?.autoLowShotsMade}</b> total
-            auto goals
+            <b>{entry.autoHighShotsMade + entry?.autoLowShotsMade}</b> auto
+            goals
           </div>
           <div>
-            <b>{entry.teleopHighShotsMade + entry.teleopLowShotsMade}</b> total
-            teleop goals
-          </div>
-          <div>
-            <b>
-              {(
-                ((entry.autoHighShotsMade +
-                  entry.autoLowShotsMade +
-                  entry.teleopHighShotsMade +
-                  entry.teleopHighShotsMade) /
-                  (entry.autoHighShotsTotal +
-                    entry.autoHighShotsMade +
-                    entry.teleopHighShotsMade +
-                    entry.teleopLowShotsTotal)) *
-                100
-              ).toFixed(2)}
-            </b>
-            % accuracy
+            <b>{entry.teleopHighShotsMade + entry.teleopLowShotsMade}</b> teleop
+            goals
           </div>
           <div>
             {entry.climbEnd - entry.climbStart === 0
