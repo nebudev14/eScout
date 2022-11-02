f28fe3fee234e397940f061a1bb4a5fbf2336554
warren yun <wyun2006@gmail.com>
2022-11-01 23:09:46 -0400

    thanks ts

--- components/scouting/filter/filter-stats.tsx
+++ components/scouting/filter/filter-stats.tsx
@@ -1,17 +1,18 @@
 import { Statistics } from "../../../util/calculate-stats";
 
 // <{ data: Entry[] }> = ({data})
-export const FilterStats: React.FC<{ stats: Statistics }> = (
-  stats,
-) => {
+export const FilterStats: React.FC<{ stats: Statistics }> = ({stats}) => {
   return (
     <div className="flex flex-col w-full px-4 py-3 bg-gray-300 rounded-lg r">
       <div className="mb-2">
-        <h1>Ball Statistics</h1>
+        <h1 className="mb-3 text-xl">
+          <b>Ball Statistics</b>
+        </h1>
+        <h1>{stats.ballStats.autoHighShotsMade} / {stats.ballStats.autoHighShotsTotal} auto high shots</h1>
       </div>
-      <div>
-        <h1>Climb Statistics</h1>
-      </div>
+      <h1 className="text-xl ">
+        <b>Climb  Statistics</b>
+        </h1>
     </div>
   );
 };
