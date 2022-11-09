e8d8977b64c3966ed3db6578fd9944a2b54b2f3d
warren yun <wyun2006@gmail.com>
2022-11-09 13:14:36 -0500

    env type for url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,7 +32,7 @@ export default withTRPC<AppRouter>({
     const url = process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/api/trpc`
       : "http://localhost:3000/api/trpc";
-
+    
     return {
       url,
       /**
