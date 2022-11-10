ce96142adf9f427ae3ff2ccb029d5d7eab17ade9
warren yun <wyun2006@gmail.com>
2022-11-09 22:55:46 -0500

    add tabs to create team modal

--- components/modals/create-team.tsx
+++ components/modals/create-team.tsx
@@ -1,7 +1,7 @@
 import { useAtom } from "jotai";
 import { createTeamModalAtom } from "../../server/atoms";
 import React, { Fragment, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
+import { Dialog, Transition, Tab } from "@headlessui/react";
 import { trpc, useMutation, useQuery } from "../../hooks/trpc";
 
 export const CreateTeamModal: React.FC = () => {
@@ -56,51 +56,80 @@ export const CreateTeamModal: React.FC = () => {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel as="div" className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl dark:bg-black rounded-2xl">
-                <Dialog.Title
-                  as="h3"
-                  className="text-lg font-medium leading-6 text-center text-gray-900"
-                >
-                  Create a team
-                </Dialog.Title>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500"></p>
-                </div>
-
-                <form onSubmit={createTeam}>
-                  <div className="mt-4 ">
-                    <h1 className="mr-2 font-semibold">Team name</h1>
-                    <input
-                      id="teamName"
-                      className="w-full p-2 border-2 rounded-lg outline-none"
-                      required
-                      autoComplete="off"
-                    />
+              <Dialog.Panel
+                as="div"
+                className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl dark:bg-black rounded-2xl"
+              >
+                <Tab.Group>
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-center text-gray-900"
+                  >
+                    <Tab.List className="grid grid-cols-2">
+                      <Tab
+                        className={({ selected }) =>
+                          selected
+                            ? "px-6 py-2 outline-none border-b-2 border-pink-600"
+                            : "px-6 py-2"
+                        }
+                      >
+                        Create
+                      </Tab>
+                      <Tab
+                        className={({ selected }) =>
+                          selected
+                            ? "px-6 py-2 outline-none border-b-2 border-pink-600"
+                            : "px-6 py-2"
+                        }
+                      >
+                        Join
+                      </Tab>
+                    </Tab.List>
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500"></p>
                   </div>
 
-                  <div className="mt-4 ">
-                    <h1 className="mr-2 font-semibold" id="teamNum">
-                      Team number
-                    </h1>
-                    <input
-                      className="w-full p-2 border-2 rounded-lg outline-none"
-                      id="teamNum"
-                      type="number"
-                      required
-                      autoComplete="off"
-                    />
-                  </div>
+                  <Tab.Panels>
+                    <Tab.Panel>
+                      <form onSubmit={createTeam}>
+                        <div className="mt-4 ">
+                          <h1 className="mr-2 font-semibold">Team name</h1>
+                          <input
+                            id="teamName"
+                            className="w-full p-2 border-2 rounded-lg outline-none"
+                            required
+                            autoComplete="off"
+                          />
+                        </div>
 
-                  <div className="mt-4">
-                    <button
-                      type="submit"
-                      className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-green-400 border border-transparent rounded-md hover:bg-purple-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={() => setIsOpen(false)}
-                    >
-                      Create
-                    </button>
-                  </div>
-                </form>
+                        <div className="mt-4 ">
+                          <h1 className="mr-2 font-semibold" id="teamNum">
+                            Team number
+                          </h1>
+                          <input
+                            className="w-full p-2 border-2 rounded-lg outline-none"
+                            id="teamNum"
+                            type="number"
+                            required
+                            autoComplete="off"
+                          />
+                        </div>
+
+                        <div className="mt-4">
+                          <button
+                            type="submit"
+                            className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-green-400 border border-transparent rounded-md hover:bg-purple-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                            onClick={() => setIsOpen(false)}
+                          >
+                            Create
+                          </button>
+                        </div>
+                      </form>
+                    </Tab.Panel>
+                    <Tab.Panel>kek</Tab.Panel>
+                  </Tab.Panels>
+                </Tab.Group>
               </Dialog.Panel>
             </Transition.Child>
           </div>
