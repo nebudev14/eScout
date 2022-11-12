a696bc3630611ce88fee2273c81035f7d9775eb6
warren yun <wyun2006@gmail.com>
2022-11-11 20:24:58 -0500

    metaware

---
+++ server/middleware/metaware.ts
@@ -0,0 +1,20 @@
+import type { Context } from "../context";
+import type { MiddlewareFunction } from "@trpc/server/dist/declarations/src/internals/middlewares";
+import { TRPCError } from "@trpc/server";
+
+interface Meta {
+  auth: boolean;
+};
+export const metaware: MiddlewareFunction<Context, Context, Meta> = ({
+  ctx,
+  next,
+  meta,
+}) => {
+  if (!ctx.session && meta?.auth) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return next({
+    ctx,
+  });
+};
