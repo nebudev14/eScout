41ae79e192d2b4596d20660a8327a82eaff9ce1d
warren yun <wyun2006@gmail.com>
2022-12-31 05:42:17 -0500

    more question rendering ahahaha

--- util/render-question-model.tsx
+++ util/render-question-model.tsx
@@ -4,6 +4,7 @@ import { BoolInput } from "../components/ui/form/bool-input";
 import { CounterInput } from "../components/ui/form/counter-input";
 import { FormInput } from "../components/ui/form/form-input";
 import { SelectInput } from "../components/ui/form/select-input";
+import { Answer } from "../types/form-types";
 
 export function renderDesiredQuestionDisplay(
   questionType: MatchQuestionType,
@@ -21,5 +22,33 @@ export function renderDesiredQuestionDisplay(
       return <FormInput label={label} id="" />;
     case "SELECT":
       return <SelectInput label={label} id="" options={options} />;
+  }
+}
+
+export function renderFormQuestion(
+  questionType: MatchQuestionType,
+  updateState: (answer: Answer) => void,
+  label: string,
+  id: string,
+  options?: string[]
+) {
+  switch (questionType) {
+    case "SCORE":
+      return <ScoreBoard label={label} id={id} updateState={updateState} />;
+    case "BOOL":
+      return <BoolInput label={label} id={id} updateState={updateState} />;
+    case "COUNTER":
+      return <CounterInput label={label} id={id} updateState={updateState} />;
+    case "INPUT":
+      return <FormInput label={label} id={id} updateState={updateState} />;
+    case "SELECT":
+      return (
+        <SelectInput
+          label={label}
+          id={id}
+          options={options}
+          updateState={updateState}
+        />
+      );
   }
 }
