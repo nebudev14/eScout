fc1dc516c26c6b1009cf6abe4b45ba78ee1c2a59
warren yun <wyun2006@gmail.com>
2023-02-26 11:20:23 -0500

    boo silly

--- pages/teams/[id]/matchscout/edit/[match_id].tsx
+++ pages/teams/[id]/matchscout/edit/[match_id].tsx
@@ -8,6 +8,7 @@ import EditMatchModal from "@components/modals/edit-match-modal";
 import { MatchFormCategory } from "@prisma/client";
 import { renderDesiredQuestionDisplay } from "@util/render-question-model";
 import { trpc } from "@util/trpc/trpc";
+import { Tab } from "@headlessui/react";
 
 const EditMatchScout: React.FC = () => {
   const router = useRouter();
@@ -31,6 +32,8 @@ const EditMatchScout: React.FC = () => {
     },
   });
 
+  const tabs = ["Form", "Stats"];
+
   return (
     <div className="min-h-screen py-1 2xl:px-52 xl:px-48 dark:text-white md:px-4">
       <BiArrowBack
@@ -42,71 +45,98 @@ const EditMatchScout: React.FC = () => {
         <h1 className="mr-auto text-3xl ">
           Editing <b>{data?.name}</b>
         </h1>
-        <button
-          className="px-4 py-2 text-white bg-pink-600 rounded-lg hover:bg-pink-700"
-          onClick={() => setIsCategoryOpen(true)}
-        >
-          +
-        </button>
       </div>
-      <div className="mx-12 mt-8 2xl:px-24 xl:px-12">
-        {data?.categories.map((category, i) => (
-          <div key={i} className="my-4">
-            <div className="flex flex-row items-center mb-4 border-b-2 dark:border-zinc-700">
-              <h1 className="py-2 mr-4 text-3xl font-semibold">
-                {category?.name}
-              </h1>
-              <BsPencilFill
-                size={20}
-                className="hover:cursor-pointer"
-                onClick={() => {
-                  setCurrentCategory(category);
-                  setIsEditOpen(true);
-                }}
-              />
-              <BsFillTrashFill
-                size={20}
-                className="ml-4 text-red-400 duration-200 hover:text-red-500 hover:cursor-pointer"
-                onClick={() => {
-                  setCurrentCategory(category);
-                  setIsDeleteOpen(true);
-                }}
-              />
-            </div>
-            <div>
-              {category.questions.map((question, j) => (
-                <div key={j} className="my-8">
-                  {renderDesiredQuestionDisplay(
-                    question.questionType,
-                    question.prompt as string,
-                    question.options
-                  )}
+      <Tab.Group>
+        <div className="flex flex-col items-start text-xl md:text-lg md:items-center">
+          <Tab.List className="grid grid-cols-3 mt-4 mb-8">
+            {tabs.map((tab, i) => (
+              <Tab
+                key={i}
+                className={({ selected }) =>
+                  selected
+                    ? "px-6 py-2 md:px-2 md:text-sm outline-none border-b-2 border-cyan-400"
+                    : "px-6 py-2 md:px-2 md:text-sm"
+                }
+              >
+                {tab}
+              </Tab>
+            ))}
+          </Tab.List>
+        </div>
+        <Tab.Panels className="mt-4">
+          <Tab.Panel>
+            <button
+              className="px-4 py-2 text-white bg-pink-600 rounded-lg hover:bg-pink-700"
+              onClick={() => setIsCategoryOpen(true)}
+            >
+              +
+            </button>
+            <div className="mx-12 mt-8 2xl:px-24 xl:px-12">
+              {data?.categories.map((category, i) => (
+                <div key={i} className="my-4">
+                  <div className="flex flex-row items-center mb-4 border-b-2 dark:border-zinc-700">
+                    <h1 className="py-2 mr-4 text-3xl font-semibold">
+                      {category?.name}
+                    </h1>
+                    <BsPencilFill
+                      size={20}
+                      className="hover:cursor-pointer"
+                      onClick={() => {
+                        setCurrentCategory(category);
+                        setIsEditOpen(true);
+                      }}
+                    />
+                    <BsFillTrashFill
+                      size={20}
+                      className="ml-4 text-red-400 duration-200 hover:text-red-500 hover:cursor-pointer"
+                      onClick={() => {
+                        setCurrentCategory(category);
+                        setIsDeleteOpen(true);
+                      }}
+                    />
+                  </div>
+                  <div>
+                    {category.questions.map((question, j) => (
+                      <div key={j} className="my-8">
+                        {renderDesiredQuestionDisplay(
+                          question.questionType,
+                          question.prompt as string,
+                          question.options
+                        )}
+                      </div>
+                    ))}
+                  </div>
                 </div>
               ))}
             </div>
-          </div>
-        ))}
-      </div>
-      <CreateMatchCategoryModal
-        isOpen={isCategoryOpen}
-        setIsOpen={setIsCategoryOpen}
-      />
-      <EditMatchModal
-        isOpen={isEditOpen}
-        setIsOpen={setIsEditOpen}
-        category={currentCategory!}
-      />
+            <CreateMatchCategoryModal
+              isOpen={isCategoryOpen}
+              setIsOpen={setIsCategoryOpen}
+            />
+            <EditMatchModal
+              isOpen={isEditOpen}
+              setIsOpen={setIsEditOpen}
+              category={currentCategory!}
+            />
 
-      <ConfirmationModal
-        action="Are you sure you want to delete this category?"
-        description="All other questions under this category will also be wiped!"
-        isOpen={isDeleteOpen}
-        setIsOpen={setIsDeleteOpen}
-        func={() => {
-          deleteCategoryMutation.mutateAsync({ entityId: currentCategory!.id });
-          setIsDeleteOpen(false);
-        }}
-      />
+            <ConfirmationModal
+              action="Are you sure you want to delete this category?"
+              description="All other questions under this category will also be wiped!"
+              isOpen={isDeleteOpen}
+              setIsOpen={setIsDeleteOpen}
+              func={() => {
+                deleteCategoryMutation.mutateAsync({
+                  entityId: currentCategory!.id,
+                });
+                setIsDeleteOpen(false);
+              }}
+            />
+          </Tab.Panel>
+          <Tab.Panel>
+            
+          </Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   );
 };
