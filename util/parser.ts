3d876154f45ba883ece6e6114c8e0dbffc60f233
warren yun <wyun2006@gmail.com>
2023-02-28 00:46:42 -0500

    idk anymore

---
+++ util/parser.ts
@@ -0,0 +1,11 @@
+export function parseOperation(operation: string) {
+  
+}
+
+
+/**
+ *  AVERAGE(CATEGORY(id, slot#)) <- Only used for team display | general stats
+ *  
+ *  DIVIDE(CATEGORY(id, slot#), CATEGORY(id, slot#)) <- Returns percentage | Can be used for match and team display
+ * 
+ */
