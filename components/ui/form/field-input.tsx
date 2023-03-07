8cc922156e06de3e30510ccee08eabdb2b166bfe
warren yun <wyun2006@gmail.com>
2023-03-07 00:49:56 -0500

    the setup begin

--- components/ui/form/field-input.tsx
+++ components/ui/form/field-input.tsx
@@ -6,7 +6,11 @@ export const FieldInput: React.FC = () => {
   return (
     <div className="py-8">
       <div className="">
-        <Image src- {...Field} className="" />
+        <Image src={Field} width={2400} height={1200} className="" onClick={(e: React.MouseEvent) => {
+          const { offsetX, offsetY } = e.nativeEvent;
+          console.log(offsetX)
+          console.log(offsetY)
+        }}/>
       </div>
     </div>
   );
