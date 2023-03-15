3bf99b30d58ff169e0a1ece09df5bbfccbdf373f
warren yun <wyun2006@gmail.com>
2023-03-15 14:03:55 -0400

    its so cool

--- components/ui/misc/field-canvas.tsx
+++ components/ui/misc/field-canvas.tsx
@@ -12,13 +12,16 @@ export const FieldCanvas: React.FC<{ height: number; width: number }> = ({
     const canvas = canvasRef?.current;
     const context = canvas?.getContext("2d");
     if (context) {
-      context.beginPath();
-      context.arc(0, 0, 10, 0, 2 * Math.PI);
-      context.fillStyle = "rgba(255, 0, 0, 1)";
-      // context.moveTo(20, 200)
-      context.fill();
+      // context.beginPath();
+      // context.arc(300, 150, 5, 0, 2 * Math.PI);
+      // context.fillStyle = "rgba(255, 0, 0, 1)";
+      // // context.moveTo(20, 200)
+      // context.fill();
     }
   });
+
+  // 300 x
+  // 150 y
 
   return (
     <div className="relative">
@@ -26,10 +29,15 @@ export const FieldCanvas: React.FC<{ height: number; width: number }> = ({
         className="absolute z-50 w-full h-full "
         onClick={(e: React.MouseEvent) => {
           const { offsetX, offsetY } = e.nativeEvent;
-          const xCoord = (e.target as HTMLElement).offsetWidth / offsetX;
-          const yCoord = (e.target as HTMLElement).offsetHeight / offsetY;
-          console.log(xCoord);
-          console.log(yCoord);
+          const xCoord = offsetX / (e.target as HTMLElement).offsetWidth;
+          const yCoord = offsetY / (e.target as HTMLElement).offsetHeight;
+          const context = canvasRef?.current?.getContext("2d");
+          context!.beginPath();
+
+          context!.arc(xCoord*300, yCoord*150, 3, 0, 2 * Math.PI);
+          context!.fillStyle = "rgba(255, 0, 0, 1)";
+          // context.moveTo(20, 200)
+          context!.fill();
           // if (isFinite(xCoord) && isFinite(yCoord)) {
           //   setNodes([...nodes, { xCoord, yCoord }]);
           // }
@@ -37,7 +45,6 @@ export const FieldCanvas: React.FC<{ height: number; width: number }> = ({
         ref={canvasRef}
       ></canvas>
       <Image src={Field} className="relative" />
-      
     </div>
   );
 };
