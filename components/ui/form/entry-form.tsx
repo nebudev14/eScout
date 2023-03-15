bf6e0475ed19982170b06d8e5678e2d4c4e6534c
warren yun <wyun2006@gmail.com>
2023-03-14 22:04:38 -0400

    update form types

--- components/ui/form/entry-form.tsx
+++ components/ui/form/entry-form.tsx
@@ -1,8 +1,6 @@
 import { renderFormQuestion } from "../../../util/render-question-model";
 import { Answer } from "../../../types/form-types";
 import React from "react";
-import { useQuery } from "../../../hooks/trpc";
-import { useSession } from "next-auth/react";
 import { EntryFormType } from "../../../types/misc-types";
 
 interface Props {
@@ -37,34 +35,48 @@ export default class EntryForm extends React.Component<Props, State> {
               slot1: "0",
               slot2: "0",
               slot3: "0",
-              slot4: []
+              slot4: [],
+              // gamepieces: [],
+              // chargeFieldNodes: [],
             });
             break;
           case "COUNTER":
             answers.push({
               questionId: q.id,
               slot1: "0",
+              slot2: "0",
               slot3: "0",
-              slot4: []
+              slot4: [],
+
             });
             break;
           case "INPUT":
-            answers.push({ questionId: q.id, slot1: "", slot2: "", slot3: "", slot4: [] });
+            answers.push({
+              questionId: q.id,
+              slot1: "",
+              slot2: "",
+              slot3: "",
+              slot4: [],
+
+            });
             break;
           case "BOOL":
             answers.push({
               questionId: q.id,
               slot1: "No",
+              slot2: "0",
               slot3: "0",
-              slot4: []
+              slot4: [],
+
             });
             break;
           case "SELECT":
             answers.push({
               questionId: q.id,
               slot1: q.options[0],
+              slot2: "0",
               slot3: "0",
-              slot4: []
+              slot4: [],
             });
             break;
         }
--- types/form-types.ts
+++ types/form-types.ts
@@ -1,7 +1,10 @@
+import { Gamepiece, ChargedFieldNode } from "@prisma/client";
 export interface Answer {
   questionId: string;
   slot1?: string;
   slot2?: string;
   slot3?: string;
   slot4?: string[];
+  gamepieces?: Gamepiece[];
+  chargeFieldNodes?: ChargedFieldNode[];
 }
