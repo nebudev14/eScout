a2a05ca5075c6d63a0d49cf0f2ac6ebad7f44784
warren yun <wyun2006@gmail.com>
2023-03-18 00:46:42 -0400

    debug :skull:

--- pages/home.tsx
+++ pages/home.tsx
@@ -2,9 +2,13 @@ import type { NextPage } from "next";
 import Protected from "@components/auth/protected";
 import NoTeams from "@components/ui/no-teams";
 import { trpc } from "@util/trpc/trpc";
+import { useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
+  const { data: session } = useSession();
+  console.log(session)
   const { data, isLoading } = trpc.user.getUser.useQuery();
+
 
   if (isLoading || !data) return <h1>Loading...</h1>;
 
