c5d379431561e1e8b449a20fb96c83ea5ffc4bd0
warren yun <wyun2006@gmail.com>
2023-03-19 00:08:38 -0400

    found the build error!

--- server/routers/user-router.ts
+++ server/routers/user-router.ts
@@ -4,14 +4,14 @@ import { getFormInclude, getUserInclude } from "../repositories/user-repo";
 
 export const userRouter = router({
   getUser: authProcedure.query(async ({ ctx }) => {
-    return await prisma.user.findUnique({
+    return await ctx.prisma.user.findUnique({
       where: { id: ctx.session.user.id },
       include: getUserInclude()
     })
   }),
 
   getForms: authProcedure.query(async ({ ctx }) => {
-    return await prisma.user.findUnique({
+    return await ctx.prisma.user.findUnique({
       where: { id: ctx.session.user.id },
       include: getFormInclude()
     })
