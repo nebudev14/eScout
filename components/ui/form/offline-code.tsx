ae1e387a58fbb1081ed17f2f45f4e51edf3020ad
warren yun <wyun2006@gmail.com>
2023-03-20 02:48:42 -0400

    QR CODE GENERATION WORKS WOO

--- components/ui/form/offline-code.tsx
+++ components/ui/form/offline-code.tsx
@@ -9,14 +9,33 @@ export const OfflineCode: React.FC<{ answers: Answer[] }> = ({ answers }) => {
     <div>
       <div className="grid grid-cols-1">
         <button
-          className="p-2 mt-2 text-lg font-semibold text-white duration-150 rounded shadow bg-cyan-500 focus:outline-none focus:shadow-outline"
+          className="p-2 mt-2 mb-4 text-lg font-semibold text-white duration-150 rounded shadow bg-cyan-500 focus:outline-none focus:shadow-outline"
           onClick={(e: React.SyntheticEvent) => {
             e.preventDefault();
             const compressedData: Uint8Array = compress(
               JSON.stringify(answers)
             );
-            console.log(compressedData);
-            console.log(compressedData.length);
+
+            const code = new QRCode();
+            console.log(compressedData.toString().length)
+            code.setTypeNumber(40);
+            code.addData(new QR8BitByte(compressedData.toString()));
+            code.make();
+            canvasRef!.current!.width = 543;
+            canvasRef!.current!.height = 543;
+            const context = canvasRef?.current?.getContext("2d");
+            context!.fillStyle = "#ffffff";
+            context!.fillRect(0, 0, 6000, 6000);
+
+            context!.fillStyle = "#000000";
+            for (var row = 0; row < code.getModuleCount(); row += 1) {
+              for (var col = 0; col < code.getModuleCount(); col += 1) {
+                if (code.isDark(row, col)) {
+                  console.log("dark")
+                  context!.fillRect(col*3.05, row*3.05, 3.05, 3.05);
+                }
+              }
+            }
           }}
         >
           Submit Offline
