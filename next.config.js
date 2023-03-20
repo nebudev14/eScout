7a696efcb441d5811035d4237181e4238b6ac7dd
warren yun <wyun2006@gmail.com>
2023-03-20 09:52:26 -0400

    sus

--- next.config.js
+++ next.config.js
@@ -9,17 +9,17 @@ const nextConfig = {
       {
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
+          // { key: "Access-Control-Allow-Credentials", value: "true" },
+          // { key: "Access-Control-Allow-Origin", value: "*" },
+          // {
+          //   key: "Access-Control-Allow-Methods",
+          //   value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          // },
+          // {
+          //   key: "Access-Control-Allow-Headers",
+          //   value:
+          //     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          // },
           { key: "Cache-Control", value: "no-store, max-age=0" },
         ],
       },
