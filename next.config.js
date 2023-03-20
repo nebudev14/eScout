bbc408ea9a1edffa342bef2891b3f7b3cef860a1
warren yun <wyun2006@gmail.com>
2023-03-20 09:52:37 -0400

    SUS

--- next.config.js
+++ next.config.js
@@ -4,27 +4,27 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          // { key: "Access-Control-Allow-Credentials", value: "true" },
-          // { key: "Access-Control-Allow-Origin", value: "*" },
-          // {
-          //   key: "Access-Control-Allow-Methods",
-          //   value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          // },
-          // {
-          //   key: "Access-Control-Allow-Headers",
-          //   value:
-          //     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          // },
-          { key: "Cache-Control", value: "no-store, max-age=0" },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       headers: [
+  //         // { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         // { key: "Access-Control-Allow-Origin", value: "*" },
+  //         // {
+  //         //   key: "Access-Control-Allow-Methods",
+  //         //   value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  //         // },
+  //         // {
+  //         //   key: "Access-Control-Allow-Headers",
+  //         //   value:
+  //         //     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  //         // },
+  //         { key: "Cache-Control", value: "no-store, max-age=0" },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
